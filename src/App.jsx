ec4eb998115b995a38dbd43443091526f9f8a40b
Juan Salazar <jsalazarv8@gmail.com>
2022-09-08 17:43:21 -0500

    Styles are added in Promotional Banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home/Home';
-import { Products } from './pages/Home/components/Products/Products.jsx';
 import { Orders } from './pages/Orders/Orders';
+import { Navbar } from './components/Navbar/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/orders" element={<Orders />} />
