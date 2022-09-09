75f0bcf1d9c16b7ec8f323069f7fbf6ff4e4bd1e
Juan Salazar <jsalazarv8@gmail.com>
2022-09-09 13:12:50 -0500

    Styles are added on main cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,11 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+
 export const Card = ({
   image = '',
   category,
   title,
+  description,
   price,
   color = 'bg_gradient_shark',
   onClickAdd = () => null,
@@ -17,11 +21,15 @@ export const Card = ({
           <span className="card__content_category">{category}</span>
           <div className="card__content_info">
             <span className="card__content_info_title">{title}</span>
+            <p className="card__content_info_description">{description}</p>
           </div>
           <div className="button">
-            <span className="card__content_info_price">{price}</span>
-            <button className="card__content_info_price" onClick={onClickAdd}>
-              Buy
+            <span className="card__content_info_price">${price}</span>
+            <button
+              className="card__content_info_price bg-amber-500 border-amber-600 text-black shadow-3xl hover:bg-amber-600"
+              onClick={onClickAdd}>
+              Add to cart
+              <FontAwesomeIcon className="ml-3" icon={faShoppingCart} />
             </button>
           </div>
         </div>
