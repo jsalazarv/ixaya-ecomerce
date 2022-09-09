d36f1b49f0b63f17315bf1f8dd37900dda853c57
Juan Salazar <jsalazarv8@gmail.com>
2022-09-09 11:21:29 -0500

    Styles added to main banner

--- src/pages/Home/components/Banner/Banner.jsx
+++ src/pages/Home/components/Banner/Banner.jsx
@@ -10,11 +10,13 @@ export const Banner = () => {
           <h1 className="banner__content__title">
             <b>HEADPHONE</b>
           </h1>
-          <button
-            type="button"
-            className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
-            Shop By Category
-          </button>
+          <div className="text-center lg:text-left md:mt-6">
+            <button
+              type="button"
+              className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+              Shop By Category
+            </button>
+          </div>
           <img className="banner__content__image" src={imageBanner} alt="" />
         </div>
       </div>
