14ea7a58eac0fdd74aa72cdb8fffc99ee8862c47
Juan Salazar <jsalazarv8@gmail.com>
2022-09-09 21:42:54 -0500

    Styles are added in the button to remove item from the cart

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -4,7 +4,7 @@ import {
   removeItem,
 } from '../../store/cart/index.js';
 import { useDispatch } from 'react-redux';
-import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faMinus, faPlus, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const ProductItem = ({
@@ -15,56 +15,59 @@ export const ProductItem = ({
   const dispatch = useDispatch();
 
   return (
-    <div className="product-container">
-      <div className="image-container">
-        <img className="product__image" src={product.image_url} alt="" />
-      </div>
-      <div className="product__body">
-        <h2 className="product__body__title">{product.title}</h2>
-        <p>{product.short_description}</p>
-        <div className="mt-3">
-          <div className="flex justify-between">
-            <div>Quantity:</div>
-            <div className="counter">
-              {incrementable && (
-                <button
-                  className="counter__button"
-                  onClick={() => dispatch(removeItem(product))}>
-                  <FontAwesomeIcon icon={faMinus} />
-                </button>
-              )}
-              <span className="counter__qty">{product.qty}</span>
-              {incrementable && (
-                <button
-                  className="counter__button"
-                  onClick={() => dispatch(addItem(product))}>
-                  <FontAwesomeIcon icon={faPlus} />
-                </button>
-              )}
-            </div>
-          </div>
-          <div className="flex justify-between">
-            Unit price:
-            <span>${product.price}</span>
-          </div>
-          <div className="flex justify-between">
-            Discount:
-            <span className="discount">- ${product.discount}</span>
-          </div>
-        </div>
-      </div>
-      <div className="product__info">
+    <>
+      <div className="delete-item">
         {deletable && (
           <button onClick={() => dispatch(deleteCheckoutItem(product))}>
-            delete
+            <FontAwesomeIcon className="icon" icon={faTrash} />
           </button>
         )}
-
-        <div className="flex justify-between">
-          Subtotal:
-          <span>${product.total}</span>
+      </div>
+      <div className="product-container">
+        <div className="image-container">
+          <img className="product__image" src={product.image_url} alt="" />
+        </div>
+        <div className="product__body">
+          <h2 className="product__body__title">{product.title}</h2>
+          <p>{product.short_description}</p>
+          <div className="mt-3">
+            <div className="flex justify-between">
+              <div>Quantity:</div>
+              <div className="counter">
+                {incrementable && (
+                  <button
+                    className="counter__button"
+                    onClick={() => dispatch(removeItem(product))}>
+                    <FontAwesomeIcon icon={faMinus} />
+                  </button>
+                )}
+                <span className="counter__qty">{product.qty}</span>
+                {incrementable && (
+                  <button
+                    className="counter__button"
+                    onClick={() => dispatch(addItem(product))}>
+                    <FontAwesomeIcon icon={faPlus} />
+                  </button>
+                )}
+              </div>
+            </div>
+            <div className="flex justify-between">
+              Unit price:
+              <span>${product.price}</span>
+            </div>
+            <div className="flex justify-between">
+              Discount:
+              <span className="discount">- ${product.discount}</span>
+            </div>
+          </div>
+        </div>
+        <div className="product__info">
+          <div className="flex justify-between">
+            Subtotal:
+            <span>${product.total}</span>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
